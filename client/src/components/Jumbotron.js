d62c658469caa602596bce3848da1fafa8da86a6
John Garrett <infantrygarrett@icloud.com>
2019-04-30 21:16:04 -0500

    jumbotron

---
+++ client/src/components/Jumbotron.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div className="container mt-3 text-center">
+      <div className="jumbotron jumbotron-fluid bg-info">
+        <h1 className="display-4 font-weight-bold text-white">
+          Google Books Search
+        </h1>
+        <h3 className="font-weight-bold text-white">
+          Search for and Save Books that you like.
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
